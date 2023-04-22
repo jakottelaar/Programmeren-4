62a1c927c72dc6000dc25dfb5b9e4cb8bc7ff67f
jakottelaar <jarno.kottelaar@gmail.com>
2023-04-22 22:43:09 +0200

    Separated the dummy data

---
+++ API1/data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    id: 0,
+    firstName: "Jarno",
+    lastName: "Kottelaar",
+    street: "Teststreet 123",
+    city: "TestCity",
+    isActive: true,
+    email: "jarno@mail.com",
+    password: "password123",
+    phoneNumber: "06 123456789",
+  },
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Guy",
+    street: "Teststreet 456",
+    city: "TestCity",
+    isActive: true,
+    email: "j.doe@server.com",
+    password: "secret",
+    phoneNumber: "06 987654321",
+  },
+];
+
+module.exports = data;
