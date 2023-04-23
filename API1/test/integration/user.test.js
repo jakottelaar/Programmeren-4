bfefbbf662248462699e123df0e7e8cabaec7fb1
jakottelaar <jarno.kottelaar@gmail.com>
2023-04-23 19:39:57 +0200

    Added test for UC-202-1

--- API1/test/integration/user.test.js
+++ API1/test/integration/user.test.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const server = require("../../app");
+const exp = require("constants");
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -31,6 +32,25 @@ describe("UC-201 Registreren als een nieuwe gebruiker", () => {
 
         expect(status).to.equal(200);
         expect(message).to.be.a("string").that.contains("registered");
+
+        done();
+      });
+  });
+});
+
+describe("UC-202 Opvragen van overzicht van users", () => {
+  it("TC-202-1 Toon alle gebruikers (minimaal 2)", (done) => {
+    chai
+      .request(server)
+      .get("/api/user")
+      .end((err, res) => {
+        expect(err).to.be.null;
+
+        console.log(res.body);
+        expect(res.body).to.be.an("array");
+        let { status, result } = res.body;
+
+        expect(res.status).to.equal(200);
 
         done();
       });
