3057e658a74dbcf16bbb9c93ae69a9f38c5de0fc
jakottelaar <jarno.kottelaar@gmail.com>
2023-05-11 17:41:12 +0200

    Added regrex and error handling to update user endpoint

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -175,6 +175,49 @@ const userController = {
     const userId = parseInt(req.params.userId);
     const updatedUser = req.body;
 
+    const schema = Joi.object({
+      firstName: Joi.string().required(),
+      lastName: Joi.string().min(2).required(),
+      street: Joi.string().allow("").required(),
+      city: Joi.string().allow("").required(),
+      emailAddress: Joi.string()
+        .pattern(
+          new RegExp(/^[a-zA-Z]\.[a-zA-Z]{2,}@([a-zA-Z]{2,}\.[a-zA-Z]{2,3})$/)
+        )
+        .required()
+        .messages({
+          "string.pattern.base": `Email address is not valid`,
+        }),
+      password: Joi.string()
+        .pattern(/^(?=.*[A-Z])(?=.*\d).{8,}$/)
+        .required()
+        .messages({
+          "string.empty": `Password address cannot be empty`,
+          "any.required": `Password is required`,
+          "string.pattern.base": `Password is not valid. It should be at least 8 characters and contain at least one uppercase letter and one digit.`,
+        }),
+      phoneNumber: Joi.string()
+        .length(10)
+        .pattern(/^06[-\s]?\d{8}$/)
+        .required()
+        .messages({
+          "string.empty": `Phone number cannot be empty`,
+          "any.required": `Phone number is required`,
+          "string.pattern.base": `Phone number is not valid. It should start with '06' and be followed by 8 digits.`,
+          "string.length": `Phone number should be 10 digits long.`,
+        }),
+    });
+
+    const { error, value } = schema.validate(updatedUser);
+
+    if (error) {
+      res.status(400).json({
+        status: 400,
+        message: error.details[0].message,
+      });
+      return;
+    }
+
     let sqlStatement = "UPDATE user SET ? WHERE id = ?";
 
     pool.query(
@@ -195,11 +238,29 @@ const userController = {
           });
         } else {
           logger.info(`Updated user by id: ${userId}`);
-          res.status(200).json({
-            status: 200,
-            message: "Updated user",
-            data: results,
-          });
+          // Fetch the updated user information from the database
+          let selectStatement = "SELECT * FROM user WHERE id = ?";
+          pool.query(
+            selectStatement,
+            userId,
+            function (error, results, fields) {
+              if (error) {
+                console.log(error);
+                res.status(500).json({
+                  status: 500,
+                  message: "Error retrieving updated user information",
+                  error: error,
+                });
+              } else {
+                const updatedUserInfo = results[0]; // Assuming there is only one updated user row
+                res.status(200).json({
+                  status: 200,
+                  message: "Updated user",
+                  data: updatedUserInfo,
+                });
+              }
+            }
+          );
         }
       }
     );
