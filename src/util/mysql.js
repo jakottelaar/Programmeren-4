a3fdb419a3f6da458872b9b710b001782d3ebb0d
XxRoguex <jarno.kottelaar@gmail.com>
2023-05-11 10:28:45 +0200

    Updated createNewUser endpoint
    Changed var email to emailAddress

--- src/util/mysql.js
+++ src/util/mysql.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "sqlPassword1234",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "share-a-meal-db",
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
