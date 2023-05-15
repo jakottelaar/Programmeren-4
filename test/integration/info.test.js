ef5568508b9ba1959d1a96bd9ca8d257fb2ef00c
jakottelaar <jarno.kottelaar@gmail.com>
2023-05-15 16:26:54 +0200

    Created tests for api info endpoint

---
+++ test/integration/info.test.js
@@ -0,0 +1,46 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../app");
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe("UC-102 Informatie opvragen", function () {
+  it("TC-102-1 - Server info should return successful information", (done) => {
+    chai
+      .request(server)
+      .get("/api/info")
+      .end((err, res) => {
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("status").to.equal(201);
+        expect(res.body).to.have.property("message");
+        expect(res.body).to.have.property("data");
+
+        const { data, message } = res.body;
+
+        expect(data).to.be.an("object");
+        expect(data).to.have.property("studentName").to.equal("Jarno");
+        expect(data).to.have.property("studentNumber").to.equal(2176550);
+
+        done();
+      });
+  });
+
+  it("TC-102-2 - Server should return valid error when endpoint does not exist", (done) => {
+    chai
+      .request(server)
+      .get("/api/doesnotexist")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body).to.be.an("object");
+
+        const { data, message, status } = res.body;
+
+        expect(status).to.equal(404);
+        expect(message).to.be.a("string").that.is.equal("Endpoint not found");
+        expect(data).to.be.an("object");
+
+        done();
+      });
+  });
+});
